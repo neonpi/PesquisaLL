e555bcb6aa45215d9254510e81baf890dc292e79
Rafael Freesz <rafaelfreesz@gmail.com>
2025-01-20 11:16:44 -0300

    Corrigindo a instância quebrada

--- main.cpp
+++ main.cpp
@@ -15,39 +15,41 @@ int main()
     cout<<"LOADING FINISHED"<<endl;
 
     Config* config = new Config(30,0.03);
+    config->print();
     //Config* config = new Config(30,0.05);
 
     //Utils::print_output(s);
     cout<<"RUNNING EXPERIMENTS"<<endl;
     for(Instance* instance: instances) {
-        if (instance->inst_name != "R103_co_100.txt") {
-            cout<<"Instance "<< instance->inst_name<<endl;
-            Stats* stats = new Stats(instance, config);
 
-            for(int i=0;i<config->runs;i++) {
+        cout<<"Instance "<< instance->inst_name<<endl;
+        Stats* stats = new Stats(instance, config);
 
-                srand(config->seeds.at(i));
-                //cout<<config->seeds.at(i)<<endl;
-                //srand(0);
+        for(int i=0;i<config->runs;i++) {
 
-                Search* search = new Search(instance,config);
-                clock_t time = clock();
-                search->run();
-                time = clock() - time;
+            srand(config->seeds.at(i));
+            //cout<<config->seeds.at(i)<<endl;
+            //srand(26142);
 
-                stats->set_result(search->total_cost,((double) time / CLOCKS_PER_SEC));
-                Utils::print_route_file(search,i==0, config->seeds.at(i));
-                search->print_is_viable();
-                delete search;
+            Search* search = new Search(instance,config);
+            clock_t time = clock();
+            search->run();
+            time = clock() - time;
+
+            stats->set_result(search->total_cost,((double) time / CLOCKS_PER_SEC));
+            Utils::print_route_file(search,i==0, config->seeds.at(i));
+            search->print_is_viable();
+            delete search;
 
 
-            }
 
-            stats->finish_stats();
-            cout<<"AVG_COST: "<<stats->avg_cost<<" - AVG_TIME: "<<stats->avg_time<<" - BEST_COST: "<<stats->best_cost<<" - BEST_TIME: "<<stats->best_time<<endl;
-            Utils::print_stats_file(stats);
-            delete stats;
         }
+
+        stats->finish_stats();
+        cout<<"AVG_COST: "<<stats->avg_cost<<" - AVG_TIME: "<<stats->avg_time<<" - BEST_COST: "<<stats->best_cost<<" - BEST_TIME: "<<stats->best_time<<endl;
+        Utils::print_stats_file(stats);
+        Utils::print_final_stats(stats);
+        delete stats;
 
     }
     delete config;
